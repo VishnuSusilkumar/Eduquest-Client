2f0811a92283929ed3eb882aa517eeaa5d75cd99
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-09-27 15:38:55 +0530

    changes in userauth

--- src/hooks/userAuth.tsx
+++ src/hooks/userAuth.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from "react-redux";
 
-export default function UserAuth() {
+export default function useUserAuth() {
   const { user } = useSelector((state: any) => state.auth);
   if (user) {
     return true;
