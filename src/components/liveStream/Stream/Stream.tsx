ef118fb67c0444ce3c6f32454feba625539ee919
VishnuSusilkumar <vishnususilkumar10@gmail.com>
2024-12-06 13:52:37 +0530

    updated userid in getStream

--- src/components/liveStream/Stream/Stream.tsx
+++ src/components/liveStream/Stream/Stream.tsx
@@ -21,8 +21,8 @@ import {
 
 const apiKey = "mmhfdzb5evj2";
 const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0dyZWVkbyIsInVzZXJfaWQiOiJHcmVlZG8iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczMTk4NDg1MCwiZXhwIjoxNzMyNTg5NjUwfQ.Ah0MQGHHE_6CZ_XXWiOl6sDjuq2neY8FUqbhGVNMVk8";
-const userId = "Greedo";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1NhdmFnZV9PcHJlc3MiLCJ1c2VyX2lkIjoiU2F2YWdlX09wcmVzcyIsInZhbGlkaXR5X2luX3NlY29uZHMiOjYwNDgwMCwiaWF0IjoxNzMzNDczMzI1LCJleHAiOjE3MzQwNzgxMjV9.iDsNGU4qkb7hJW3kfUe0wD1r5GsyijN8AIIEPppOVns";
+const userId = "Savage_Opress";
 
 const user: User = {
   id: userId,
